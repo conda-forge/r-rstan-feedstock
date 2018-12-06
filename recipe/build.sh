a0a6a5b4b693db546c47e03e6e1dc9e748476283
Devon Ryan <dpryan79@users.noreply.github.com>
2018-12-06 13:00:49 +0100

    Inject a compitler on windows.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [[ $target_platform =~ win*]] ; then
+  mkdir -p ~/R
+  echo -e "CXX14=vs2015" > ~/R/.makevars
+fi
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
   $R CMD INSTALL --build .
