02912aa43ca8543184a8493da83bb8be014d99ab
Devon Ryan <dpryan79@gmail.com>
2018-12-06 20:58:01 +0100

    See if patching works on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,2 @@
-mkdir -p ~/R
-echo -e "CXX14 = vs2015\n" > ~/R/Makevars
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-if [[ $target_platform == win-64 ]]; then
-  mkdir -p ~/R
-  echo -e "CXX14 = vs2015\n" >> src/Makevars.win
-fi
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
   $R CMD INSTALL --build .
