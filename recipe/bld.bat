e372f2c4718078ef507aa5d50c53c9da09766eba
Devon Ryan <dpryan79@gmail.com>
2018-12-06 22:26:50 +0100

    foo

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,4 @@
-cat src/Makevars.win
+mkdir -p ~/.R
+echo -e "CXX14=\"g++\"\nCXX14FLAGS=\"-O2 -Wall\"\nCXX14STD=-std=gnu1y" > ~/.R/Makevars.win
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
