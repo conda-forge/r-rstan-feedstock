b607d38eeb0b4d39f200ffc17d35c90c0f40d8fc
Devon Ryan <dpryan79@gmail.com>
2018-12-06 23:22:34 +0100

    again

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,7 @@
 mkdir %HOME%\.R
-echo -e "CXX14=g++\r\nCXX14FLAGS=-O2 -Wall\r\nCXX14STD=-std=gnu1y" > %HOME%\.R\Makevars.win
+(echo CXX14=g++
+CXX14FLAGS=-O2 -Wall
+CXX14STD=-std=gnu1y) > %HOME%\.R\Makevars.win
 cat %HOME%\.R\Makevars.win
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
