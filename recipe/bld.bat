d12f861e223b7b9ed8114396a3b632dc56ded49c
Daniel Bast <2790401+dbast@users.noreply.github.com>
2018-12-10 22:05:19 +0100

    Set CXX14 flags for windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,11 @@
+mkdir "%USERPROFILE%\.R"
+(
+echo CXX14 = g++
+echo CXX14STD = -std=c++14
+)>"%USERPROFILE%\.R\Makevars.win"
+type "%USERPROFILE%\.R\Makevars.win"
+
+g++ --version
+
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
