abe19466ad4063ed174ea06c90fb4a12bd6107f1
Daniel Bast <2790401+dbast@users.noreply.github.com>
2018-12-18 01:01:23 +0100

    next try

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,5 @@
-mkdir "%USERPROFILE%\.R"
-(
-echo CXX14 = g++
-echo CXX14STD = -std=c++14
-)>"%USERPROFILE%\.R\Makevars.win"
-type "%USERPROFILE%\.R\Makevars.win"
-
-g++ --version
+set "CXX_STD= CXX14"
+set "CXXFLAGS= -MD -GL -std=c++14"
 
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
