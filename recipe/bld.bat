08f5f3b9a92bf297549a8c88f8260d58fa6def89
Brian Ward <bward@flatironinstitute.org>
2024-08-16 11:53:44 -0400

    Reset bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,2 @@
-rem The system Makeconf file takes priority over Makevars.win in the src dir.
-rem In conda's system Makeconf file, CXX14 is set to nothing, causing the
-rem compilation to break, even when it is set properly in Makevars.win
-rem So we append the proper Makevars to the system Makeconf here
-set MAKECONF_FILE="%BUILD_PREFIX%\lib\R\etc\x64\Makeconf"
-copy "%MAKECONF_FILE%" "%MAKECONF_FILE%.old"
-echo CXX_STD = CXX17 >> "%MAKECONF_FILE%"
-
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
-
-rem Set Makeconf back to what it was before
-move /Y "%MAKECONF_FILE%.old" "%MAKECONF_FILE%"
