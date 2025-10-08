0c71943736d07fe40e48124892561772c2963a0f
Daniel Nachun <daniel.nachun@gmail.com>
2025-10-07 21:18:23 -0700

    verbose output

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,10 +2,12 @@
 
 # override dynamic path with known path
 sed -i.bak 's|STANHEADERS_SRC =.*|STANHEADERS_SRC = $$PREFIX/lib/R/library/StanHeaders/include/src|' src/Makevars
+
 # Fix flags for cross-compilation
 if [[ ${CONDA_BUILD_CROSS_COMPILATION:-0} == 1 ]]; then
     sed -i.bak "s?\${R_HOME}?${BUILD_PREFIX}?" src/Makevars
     sed -i.bak "s?tail -n 1?tail -n 1 | sed -i 's|${BUILD_PREFIX}|${PREFIX}|'?" src/Makevars
+    cat Makevars
 fi
 
 export TBB_LIB=${PREFIX}/lib
